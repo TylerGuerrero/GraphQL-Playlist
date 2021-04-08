57cb284339f831db484fbedb0768c92efdd6bb23
Tyler <guerrero.tyler3@gmail.com>
2021-04-08 16:44:04 -0400

    Added GraphQl Root Schema

--- server/index.js
+++ server/index.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+
 // init server
 const app = express();
 
-
 // creates a graphql http server
 app.use('/graphql', graphqlHTTP({
-    schema: schema
+    schema: schema,
+    graphiql: true
 }));
 
 const PORT = process.env.PORT || 4000;
-app.listen(() => {
+app.listen(PORT, () => {
     console.log(`Server running on Port: ${PORT}`);
 })
